1e66114b5e6e9e6f5738c71a45fb00a50a24b561
Mau Lomba <mauriciolombard@liamdeMacBook-Air.local>
2018-07-13 11:21:20 -0500

    displays bounding box

--- src/App.js
+++ src/App.js
@@ -41,7 +41,17 @@ class App extends Component {
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
-    console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box});
+    console.log(box);
   }
 
   onInputChange = (event) => { 
@@ -52,7 +62,7 @@ class App extends Component {
     this.setState({imageUrl:this.state.input});
     app.models
     .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-    .then(response => this.calculateFaceLocation(response))
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
     .catch(err => console.log(err));
   };
 
@@ -64,7 +74,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
       </div>
     );
   }
