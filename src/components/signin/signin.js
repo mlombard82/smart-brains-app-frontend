177f03a5b34368226e588aa5af5abec80df3955d
Mau Lomba <mauriciolombard@liamdeMacBook-Air.local>
2018-07-13 14:18:14 -0500

    Refactor signin onClick

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -17,7 +17,7 @@ const signin = ({onRouteChange}) => {
                     </div>
                 </fieldset>
                 <div className="">
-                    <input  onClick={onRouteChange} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+                    <input  onClick={() => onRouteChange('home')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
                 </div>
                 <div className="lh-copy mt3">
                 <a href="#0" className="f6 link dim black db">Register</a>
