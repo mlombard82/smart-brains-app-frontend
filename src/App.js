dfe16571ca3628fdf8fd195a80c0664c19dddad8
Mau Lomba <mauriciolombard@liamdeMacBook-Air.local>
2018-07-13 16:38:32 -0500

    destructuring

--- src/App.js
+++ src/App.js
@@ -80,19 +80,20 @@ class App extends Component {
   }
 
   render(){
+    const {isSignedIn, route, imageUrl, box} = this.state; //destructuring
     return (
       <div className="App">
         <Particles className='particles' params={{particlesOptions}}/>
-        <Navigation onRouteChange={this.onRouteChange} isSignedIn={this.state.isSignedIn} />
-        { this.state.route === 'home'
+        <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn} />
+        { route === 'home'
           ? <div>
               <Logo />
               <Rank />
               <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-              <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
+              <FaceRecognition imageUrl={imageUrl} box={box} />
             </div>
           : (
-            this.state.route === 'signin'
+            route === 'signin'
             ? <Signin onRouteChange={this.onRouteChange} />
             : <Register onRouteChange={this.onRouteChange} />
           )
