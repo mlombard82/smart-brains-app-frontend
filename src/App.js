b187fd0b1a75b22b93137fb9ce389b16986fb336
Mau Lomba <mauriciolombard@liamdeMacBook-Air.local>
2018-07-19 21:36:21 -0500

    add a catch statement. minor refactoring

--- src/App.js
+++ src/App.js
@@ -91,10 +91,9 @@ class App extends Component {
           })
         }).then(response => response.json())
           .then(count => {
-            this.setState(Object.assign(this.state.user, {
-              entries:count
-            }))
+            this.setState(Object.assign(this.state.user, {entries:count}))
           })
+          .catch(console.log)
       }
       this.displayFaceBox(this.calculateFaceLocation(response))
     })
